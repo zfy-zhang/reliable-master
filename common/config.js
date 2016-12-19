8fb04994040ca78181ce8862649f99ee4223754d
xiaomingstudy <1059008136@qq.com>
2016-12-19 13:34:22 +0800

    路径

--- common/config.js
+++ common/config.js
@@ -21,6 +21,7 @@ const defaultCfg = {
     jobstart:'http://192.1.1.7:9090/demo/services/wsdevice/status/',
     //slave上传脚本路径
     upresult:'http://localhost:3333/api/matc/result',
+
   },
   site: {
     title: 'Reliable',
