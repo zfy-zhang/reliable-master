11bffc7bb745ddbe533dea90f48031fd2101aa82
CodeToSurvive1 <lixiaojiao_hit@163.com>
2016-12-26 14:01:38 +0800

    添加手工测试代码

--- core/slave/manager.js
+++ core/slave/manager.js
@@ -14,196 +14,207 @@ const models = require('../../common/models');
 const Device = models.Device;
 
 const STATUS = {
-  AVAILABLE: 'available',
-  BUSY: 'busy',
-  ACK: 'ack'
+    AVAILABLE: 'available',
+    BUSY: 'busy',
+    ACK: 'ack'
 };
 
 class Manager {
-  constructor() {
-    this.slaves = {};
-    this.init();
-  }
-
-  init() {
-    this.monitor();
-    _.setArchiveConfig('slaves', this.slaves);
-  }
-
-  bind(data) {
-    const hostname = data.sysInfo.hostname;
-
-    const sock = zmq.socket('pair');
-    const address = `tcp://${data.ip}:${data.port}`;
-
-    logger.debug(`connect to tcp://${data.ip}:${data.port}`);
-
-    sock.connect(address);
-    sock.on('message', (e) => {
-      const data = JSON.parse(e.toString());
-      const _hostname = data.sysInfo.hostname;
-      // data
-      //
-      // {
-      //    type: 'ack', 'task', 'monitor'
-      //    sysInfo
-      //    timestamp
-      // }
-
-      switch (data.type) {
-        case 'ack':
-          this.slaves[hostname || _hostname].status = STATUS.AVAILABLE;
-          events.sendToSingleCluster({
-            message: events.EVENTS.SLAVE_ONLINE,
-            data: this.getAvailableSlaves()
-          });
-          break;
-        case 'task':
-          const id = parseInt(Math.random() * Object.keys(cluster.workers).length + 1, 10);
-          cluster.workers[id].send({
-            message: 'archive',
-            data: data
-          });
-          logger.debug('%s<----- zmq message %s%j', EOL, EOL, data);
-          this.slaves[hostname || _hostname].sysInfo = data.sysInfo;
-          // this.slaves[hostname || _hostname].status = data.status;
-          break;
-        case 'monitor':
-          this.slaves[hostname || _hostname].sysInfo = data.sysInfo;
-          this.slaves[hostname || _hostname].timestamp = data.timestamp;
-          _.setArchiveConfig('slaves', this.slaves);
-          break;
-      }
-    });
-    sock.monitor(500, 0);
-    sock.on('disconnect', () => {
-      sock.disconnect(address);
-      logger.debug('%s is disconnected, original address is %s.', hostname, address);
-      delete this.slaves[hostname];
-      _.setArchiveConfig('slaves', this.slaves);
-
-      events.sendToSingleCluster({
-        message: events.EVENTS.LOST_SLAVE,
-        data: this.getAvailableSlaves()
-      });
-    });
-    data.timestamp = Date.now();
-    data.sock = sock;
-    this.slaves[hostname] = data;
-    _.setArchiveConfig('slaves', this.slaves);
-  }
-
-  getAvailableSlaves(runiOS) {
-    const availableSlaves = _.values(this.slaves).filter(slave => {
-      const isAvl = slave.status === STATUS.AVAILABLE;
-
-      if (runiOS) {
-        return isAvl && runiOS === slave.supportiOS;
-      }
-
-      return isAvl;
-    });
-
-    if (!availableSlaves.length) {
-      logger.debug('no available slaves to dispatch');
-      return;
+    constructor() {
+        this.slaves = {};
+        this.init();
     }
 
-    return availableSlaves.reduce((previousSlave, currentSlave) => {
-      return previousSlave.sysInfo.memory > currentSlave.sysInfo.memory ? previousSlave : currentSlave;
-    });
-  }
-
-  dispatch(data) {
-    if (!this.isReady()) {
-      logger.debug('no slave to dispatch');
-      return;
+    init() {
+        this.monitor();
+        _.setArchiveConfig('slaves', this.slaves);
     }
 
-    const availableSlave = this.getAvailableSlaves(data.runiOS);
+    bind(data) {
+        const hostname = data.sysInfo.hostname;
 
-    if (availableSlave) {
-      logger.debug('%s----->> dispatch to %s with %s %j', EOL, availableSlave.sysInfo.hostname, EOL, data);
-      // availableSlave.status = STATUS.BUSY;
-      console.log('devices:',data.serialNumber);
-      var serialNumber = data.serialNumber;
+        const sock = zmq.socket('pair');
+        const address = `tcp://${data.ip}:${data.port}`;
 
-      if(serialNumber){
-        var device = new Device();
-        //根据序列号查询数据库中是否存在该设备
+        logger.debug(`connect to tcp://${data.ip}:${data.port}`);
 
-        co(function *() {
-          const deviceDb =  yield device.getBySerialNumber(serialNumber);
+        sock.connect(address);
+        sock.on('message', (e) => {
+            const data = JSON.parse(e.toString());
+            const _hostname = data.sysInfo.hostname;
+            // data
+            //
+            // {
+            //    type: 'ack', 'task', 'monitor'
+            //    sysInfo
+            //    timestamp
+            // }
 
-          //如果设备可用，则发送任务
-          if(deviceDb!=null&&deviceDb.status==1){
-            console.log('device is available:',serialNumber);
-            availableSlave.sock.send(JSON.stringify(data));
+            switch (data.type) {
+                case 'ack':
+                    this.slaves[hostname || _hostname].status = STATUS.AVAILABLE;
+                    events.sendToSingleCluster({
+                        message: events.EVENTS.SLAVE_ONLINE,
+                        data: this.getAvailableSlaves()
+                    });
+                    break;
+                case 'task':
+                    const id = parseInt(Math.random() * Object.keys(cluster.workers).length + 1, 10);
+                    cluster.workers[id].send({
+                        message: 'archive',
+                        data: data
+                    });
+                    logger.debug('%s<----- zmq message %s%j', EOL, EOL, data);
+                    this.slaves[hostname || _hostname].sysInfo = data.sysInfo;
+                    // this.slaves[hostname || _hostname].status = data.status;
+                    break;
+                case 'monitor':
+                    this.slaves[hostname || _hostname].sysInfo = data.sysInfo;
+                    this.slaves[hostname || _hostname].timestamp = data.timestamp;
+                    _.setArchiveConfig('slaves', this.slaves);
+                    break;
+            }
+        });
+        sock.monitor(500, 0);
+        sock.on('disconnect', () => {
+            sock.disconnect(address);
+            logger.debug('%s is disconnected, original address is %s.', hostname, address);
+            delete this.slaves[hostname];
+            _.setArchiveConfig('slaves', this.slaves);
 
-            Object.keys(cluster.workers).forEach((id) => {
-              cluster.workers[id].send({
-                message: 'dispatchSuccess',
-                data: data,
-                slave: availableSlave
-              });
+            events.sendToSingleCluster({
+                message: events.EVENTS.LOST_SLAVE,
+                data: this.getAvailableSlaves()
             });
-          }else{
-            console.log('device is unavailable:',serialNumber);
-          }
         });
-      }else{
-        console.log('该任务没有指定设备');
-
-        availableSlave.sock.send(JSON.stringify(data));
-
-        Object.keys(cluster.workers).forEach((id) => {
-          cluster.workers[id].send({
-            message: 'dispatchSuccess',
-            data: data,
-            slave: availableSlave
-          });
-        });
-      }
-    } else {
-      logger.debug('no available slave to dispatch');
+        data.timestamp = Date.now();
+        data.sock = sock;
+        this.slaves[hostname] = data;
+        _.setArchiveConfig('slaves', this.slaves);
     }
-  }
 
-  isReady() {
-    return Object.keys(this.slaves).length;
-  }
+    getAvailableSlaves(runiOS) {
+        const availableSlaves = _.values(this.slaves).filter(slave => {
+            const isAvl = slave.status === STATUS.AVAILABLE;
 
-  monitor() {
-    setInterval(() => {
-      if (!this.isReady()) {
-        logger.debug('no slave for monitor #1');
-        return;
-      }
-      for (let slave in this.slaves) {
-        this.slaves[slave].sock.send(JSON.stringify({
-          type: 'monitor',
-          timestamp: Date.now()
-        }));
-      }
-    }, 5 * 1000);
+            if (runiOS) {
+                return isAvl && runiOS === slave.supportiOS;
+            }
 
-    setInterval(() => {
-      if (!this.isReady()) {
-        logger.debug('no slave for monitor #2');
-        return;
-      }
-      var timestamp = Date.now();
+            return isAvl;
+        });
 
-      for (let slave in this.slaves) {
-        const diffTime = timestamp - this.slaves[slave].timestamp;
-        if (diffTime > 30 * 60 * 1000) {
-          delete this.slaves[slave];
-          logger.debug('%s is disconnected, due to timeout for 30mins.', slave);
-          _.setArchiveConfig('slaves', this.slaves);
+        if (!availableSlaves.length) {
+            logger.debug('no available slaves to dispatch');
+            return;
         }
-      }
-    }, 60 * 1000);
-  }
+
+        return availableSlaves.reduce((previousSlave, currentSlave) => {
+            return previousSlave.sysInfo.memory > currentSlave.sysInfo.memory ? previousSlave : currentSlave;
+        });
+    }
+
+    getSlaveByIp(runiOS, slaveIp) {
+        const availableSlaves = _.values(this.slaves).filter(slave => {
+            const isAvl = slave.ip === slaveIp;
+
+            if (runiOS) {
+                return isAvl && runiOS === slave.supportiOS;
+            }
+
+            return isAvl;
+        });
+
+        if (!availableSlaves.length) {
+            logger.debug('no available slaves to dispatch');
+            return;
+        }
+
+        return availableSlaves.reduce((previousSlave, currentSlave) => {
+            return previousSlave.sysInfo.memory > currentSlave.sysInfo.memory ? previousSlave : currentSlave;
+        });
+    }
+
+    dispatch(data) {
+        if (!this.isReady()) {
+            logger.debug('no slave to dispatch');
+            return;
+        }
+
+        const availableSlave = this.getAvailableSlaves(data.runiOS);
+
+        if (availableSlave) {
+            logger.debug('%s----->> dispatch to %s with %s %j', EOL, availableSlave.sysInfo.hostname, EOL, data);
+            // availableSlave.status = STATUS.BUSY;
+            console.log('devices:', data.serialNumber);
+            var serialNumber = data.serialNumber;
+            var slaveIp = data.slaveIp;
+
+            if (serialNumber && slaveIp) {
+                const redisSlave = this.getAvailableSlaves(data.runiOS, slaveIp);
+                redisSlave.sock.send(JSON.stringify(data));
+
+                Object.keys(cluster.workers).forEach((id) => {
+                    cluster.workers[id].send({
+                        message: 'dispatchSuccess',
+                        data: data,
+                        slave: availableSlave
+                    });
+                });
+
+            } else {
+                console.log('该任务没有指定设备');
+
+                availableSlave.sock.send(JSON.stringify(data));
+
+                Object.keys(cluster.workers).forEach((id) => {
+                    cluster.workers[id].send({
+                        message: 'dispatchSuccess',
+                        data: data,
+                        slave: availableSlave
+                    });
+                });
+            }
+        } else {
+            logger.debug('no available slave to dispatch');
+        }
+    }
+
+    isReady() {
+        return Object.keys(this.slaves).length;
+    }
+
+    monitor() {
+        setInterval(() => {
+            if (!this.isReady()) {
+                logger.debug('no slave for monitor #1');
+                return;
+            }
+            for (let slave in this.slaves) {
+                this.slaves[slave].sock.send(JSON.stringify({
+                    type: 'monitor',
+                    timestamp: Date.now()
+                }));
+            }
+        }, 5 * 1000);
+
+        setInterval(() => {
+            if (!this.isReady()) {
+                logger.debug('no slave for monitor #2');
+                return;
+            }
+            var timestamp = Date.now();
+
+            for (let slave in this.slaves) {
+                const diffTime = timestamp - this.slaves[slave].timestamp;
+                if (diffTime > 30 * 60 * 1000) {
+                    delete this.slaves[slave];
+                    logger.debug('%s is disconnected, due to timeout for 30mins.', slave);
+                    _.setArchiveConfig('slaves', this.slaves);
+                }
+            }
+        }, 60 * 1000);
+    }
 }
 
 module.exports = Manager;
--- web/controllers/api/matc/devices.js
+++ web/controllers/api/matc/devices.js
@@ -1,9 +1,7 @@
 'use strict';
 var request = require('../../../../common/utils/request');
 var helper = require('../../../../common/utils/helper');
-var co = require('co');
 const models = require('../../../../common/models');
-
 const Device = models.Device;
 const Slave = models.Slave;
 
@@ -14,125 +12,132 @@ const Slave = models.Slave;
  */
 function *refreshAllDevicesBySlaves() {
 
-    //获取redis中的slaves缓存
-    var redisSlaves = yield helper.getArchiveConfig('slaves');
+    try {
 
-    if (Object.keys(redisSlaves || {}).length != 0) {
+        //获取redis中的slaves缓存
+        var redisSlaves = yield helper.getArchiveConfig('slaves');
 
-        var result = [];
+        if (Object.keys(redisSlaves || {}).length != 0) {
 
-        redisSlaves = helper.values(redisSlaves);
+            var result = [];
 
-        for (var i = 0; i < redisSlaves.length; i++) {
+            redisSlaves = helper.values(redisSlaves);
 
-            var redisSlave = redisSlaves[i];
+            for (var i = 0; i < redisSlaves.length; i++) {
 
-            var ip = redisSlave.ip;
-            var webPort = redisSlave.webPort;
+                var redisSlave = redisSlaves[i];
 
-            var url = `http://${ip}:${webPort}/devices`;
+                var ip = redisSlave.ip;
+                var webPort = redisSlave.webPort;
 
-            //查询数据库中是否已经保存过该slave
-            var slave = new Slave();
-            const dbSlave = yield slave.getByUrl(url);
+                var slaveUrl = `http://${ip}:${webPort}`;
 
-            var slaveId;
-            //如果没有保存过，则将slave保存到数据库
-            if (dbSlave == null) {
-                slave.slaveUrl = url;
-                yield slave.add();
-                slaveId = slave._id
-            } else {
-                slaveId = dbSlave._id;
-                dbSlave.status = 1;
-                dbSlave.updateById(slaveId,dbSlave);
-            }
+                //查询数据库中是否已经保存过该slave
+                var slave = new Slave();
+                const dbSlave = yield slave.getByUrl(slaveUrl);
 
-            // 查询该slave节点上的所有设备
-            var slaveDevices = yield querySlaveDeviceByUrl(url);
-
-            if (slaveDevices) {
-
-                result.push(slaveDevices);
-
-                var slaveDevicesSerialNumbers = [];
-                //循环遍历设备
-                for (var i = 0; i < slaveDevices.length; i++) {
-                    var slaveDevice = slaveDevices[i];
-                    var serialNumber = slaveDevice.serialNumber;
-                    slaveDevicesSerialNumbers.push(serialNumber);
-                    var device = new Device();
-
-                    //根据序列号查询数据库中是否存在该设备
-                    const data = yield device.getBySerialNumber(serialNumber);
-
-                    //如果存在，检查库中该设备原先是否挂靠在相同slave上，如果不是，则修改关联关系
-                    if (data != null) {
-                        console.log("查询到设备", data);
-
-                        //如果设备非使用中
-                        if (data.status != global.DEVICE_STATUS.USING) {
-                            if (slaveId != data.slaveId) {
-                                data.slaveId = slaveId;
-                            }
-                            data.status = slaveDevice.status;
-                            data.updated_at = Date.now();
-
-                            yield device.updateById(data._id, data);
-                        }
-                    } else {
-                        console.log("未查询到设备");
-
-                        console.log(slaveDevice);
-                        device.slaveId = slaveId;
-                        device.serialNumber = serialNumber;
-                        device.status = slaveDevice.status;
-                        device.brand = slaveDevice.brand;
-                        device.model = slaveDevice.model;
-                        device.releaseVersion = slaveDevice.releaseVersion;
-                        device.sdkVersion = slaveDevice.sdkVersion;
-                        device.abi = slaveDevice.abi;
-                        var screen = slaveDevice.screen;
-                        var arrays = screen.split("x");
-                        device.screenWidth = arrays[0];
-                        device.screenHeight = arrays[1];
-
-                        device.errorMessage = slaveDevice.errorMessage;
-                        yield device.add();
-
-                    }
+                var slaveId;
+                //如果没有保存过，则将slave保存到数据库
+                if (dbSlave == null) {
+                    slave.slaveUrl = slaveUrl;
+                    slave.slaveIp = ip;
+                    slave.slavePort = webPort;
+                    yield slave.add();
+                    slaveId = slave._id;
+                } else {
+                    slaveId = dbSlave._id;
+                    dbSlave.status = 1;
+                    dbSlave.updateById(slaveId, dbSlave);
                 }
 
-                //将连接成功后，又拔出的设备设置为不可用
-                var update = new Device();
-                var data = {
-                    status: global.DEVICE_STATUS.UNAVAILABLE
-                };
+                var url = slaveUrl + '/devices';
+                // 查询该slave节点上的所有设备
+                var slaveDevices = yield requestByUrl(url);
 
-                yield update.updateBySlaveId(slaveId, slaveDevicesSerialNumbers, data);
+                if (slaveDevices) {
+                    result.push(slaveDevices);
+
+                    var slaveDevicesSerialNumbers = [];
+                    //循环遍历设备
+                    for (var i = 0; i < slaveDevices.length; i++) {
+                        var slaveDevice = slaveDevices[i];
+                        var serialNumber = slaveDevice.serialNumber;
+                        slaveDevicesSerialNumbers.push(serialNumber);
+                        var device = new Device();
+
+                        //根据序列号查询数据库中是否存在该设备
+                        const data = yield device.getBySerialNumber(serialNumber);
+
+                        //如果存在，检查库中该设备原先是否挂靠在相同slave上，如果不是，则修改关联关系
+                        if (data != null) {
+                            console.log("查询到设备", data);
+
+                            //如果设备非使用中
+                            if (data.status != global.DEVICE_STATUS.USING) {
+                                if (slaveId != data.slaveId) {
+                                    data.slaveId = slaveId;
+                                }
+                                data.status = slaveDevice.status;
+                                data.updated_at = Date.now();
+
+                                yield device.updateById(data._id, data);
+                            }
+                        } else {
+                            console.log("未查询到设备");
+
+                            console.log(slaveDevice);
+                            device.slaveId = slaveId;
+                            device.serialNumber = serialNumber;
+                            device.status = slaveDevice.status;
+                            device.brand = slaveDevice.brand;
+                            device.model = slaveDevice.model;
+                            device.releaseVersion = slaveDevice.releaseVersion;
+                            device.sdkVersion = slaveDevice.sdkVersion;
+                            device.abi = slaveDevice.abi;
+                            var screen = slaveDevice.screen;
+                            var arrays = screen.split("x");
+                            device.screenWidth = arrays[0];
+                            device.screenHeight = arrays[1];
+
+                            device.errorMessage = slaveDevice.errorMessage;
+                            yield device.add();
+                        }
+                    }
+
+                    //将连接成功后，又拔出的设备设置为不可用
+                    var update = new Device();
+                    var data = {
+                        status: global.DEVICE_STATUS.UNAVAILABLE
+                    };
+
+                    yield update.updateBySlaveId(slaveId, slaveDevicesSerialNumbers, data);
+
+                }
 
             }
 
+            return true;
+        } else {
+            //如果没有slave，则将原先可用的设备全部设置为不可用
+            var update = new Device();
+            var data = {
+                status: global.DEVICE_STATUS.UNAVAILABLE
+            };
+
+            yield update.updateByStatus(data);
+
+            //如果没有slave，则将slaves全部设置为不可用
+            var update = new Slave();
+            var data = {
+                status: 2
+            };
+
+            yield update.updateByStatus(data);
+            return true;
         }
-
-        return true;
-    } else {
-        //如果没有slave，则将原先可用的设备全部设置为不可用
-        var update = new Device();
-        var data = {
-            status:  global.DEVICE_STATUS.UNAVAILABLE
-        };
-
-        yield update.updateByStatus(data);
-
-        //如果没有slave，则将slaves全部设置为不可用
-        var update = new Slave();
-        var data = {
-            status: 2
-        };
-
-        yield update.updateByStatus(data);
-        return true;
+    } catch (ex) {
+        console.log(ex);
+        return false;
     }
 }
 
@@ -150,7 +155,7 @@ function *queryValidDevices() {
  * @param slave
  * @returns {*}
  */
-function *querySlaveDeviceByUrl(url) {
+function *requestByUrl(url) {
 
     var result = yield request({
         uri: url,
@@ -164,10 +169,10 @@ function *querySlaveDeviceByUrl(url) {
         return false;
     }
 }
-function *deleteDevices(){
+function *deleteDevices() {
     var deviceId = this.params.deviceId;
     var device = new Device();
-    if (yield device.removeById(deviceId)){
+    if (yield device.removeById(deviceId)) {
         this.body = {
             success: true,
             errorMsg: '删除设备成功',
@@ -188,7 +193,7 @@ function *controlDevices() {
             yield runDevices.call(this);
             break;
         case 'stop':
-             yield stopDevices.call(this);
+            yield stopDevices.call(this);
             break;
         case 'delete':
             yield deleteDevices.call(this);
@@ -200,12 +205,34 @@ function *runDevices() {
     var device = new Device();
     var data = yield device.getById(deviceId);
     data.status = global.DEVICE_STATUS.USING;
-    if (yield device.updateById(deviceId,data)){
-        this.body = {
-            success: true,
-            errorMsg: '启用设备成功',
-            data: null
-        };
+
+    if (yield device.updateById(deviceId, data)) {
+
+        var display = data.screenWidth + 'x' + data.screenHeight;
+
+        var slave = new Slave();
+        var slaveData = yield slave.getById(data.slaveId);
+
+        try {
+            var result = yield request({
+                uri: slaveData.slaveUrl + '/devices/control_devices/' + deviceId + '/run',
+                form: {
+                    display: data.screenWidth + 'x' + data.screenHeight,
+                    serialNumber: data.serialNumber
+                },
+                method: 'post'
+            });
+            this.body = result.body;
+        } catch (e) {
+            console.log(e);
+            this.body = {
+                success: false,
+                errorMsg: '启用设备失败',
+                data: null
+            };
+        }
+
+
     } else {
         this.body = {
             success: false,
@@ -214,13 +241,15 @@ function *runDevices() {
         };
     }
 }
+
+
 //停止设备
 function *stopDevices() {
     var deviceId = this.params.deviceId;
     var device = new Device();
     var data = yield device.getById(deviceId);
     data.status = global.DEVICE_STATUS.AVAILABLE;
-    if (yield device.updateById(deviceId,data)){
+    if (yield device.updateById(deviceId, data)) {
         this.body = {
             success: true,
             errorMsg: '停止设备成功',
@@ -246,7 +275,7 @@ function *dispatch() {
             this.body = yield queryValidDevices.call(this);
             break;
         case 'control_devices':
-            this.body = yield controlDevices.call(this);
+            yield controlDevices.call(this);
             break;
     }
 
