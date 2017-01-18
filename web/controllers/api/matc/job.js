34b8d54b55d0218af18916c3b568424bf2037450
xiaomingstudy <1059008136@qq.com>
2017-01-18 13:49:30 +0800

    改造并优化master从业务系统下载脚本和应用

--- web/controllers/api/matc/job.js
+++ web/controllers/api/matc/job.js
@@ -14,108 +14,129 @@ const Project = models.Project;
 const Device = models.Device;
 const Attachment = models.Attachment;
 // 提交任务
-function *addTask() {
-  const project = new Project();
-  project.repositoryUrl = _.trim(this.request.body['repositoryUrl']);
-//   project.repositoryUrl = 'https://github.com/xiaomingstudy/macaca-test-sample.git';
-//
-  project.repositoryBranch = _.trim(this.request.body['repositoryBranch']);
-//   project.repositoryBranch = 'master';
-  var deviceId = _.trim(this.request.body['device_id']);
-  const device = new Device();
-  var devices = yield device.getById(deviceId);
+function* addTask() {
+        const project = new Project();
+        project.repositoryUrl = _.trim(this.request.body['repositoryUrl']);
+        //   project.repositoryUrl = 'https://github.com/xiaomingstudy/macaca-test-sample.git';
+        //
+        project.repositoryBranch = _.trim(this.request.body['repositoryBranch']);
+        //   project.repositoryBranch = 'master';
+        var deviceId = _.trim(this.request.body['device_id']);
+        const device = new Device();
+        var devices = yield device.getById(deviceId);
 
-  project.serialNumber = devices.serialNumber;
+        project.serialNumber = devices.serialNumber;
 
-  project.resultUrl = _.trim(this.request.body['result_url']);
-  project.scriptUrl = _.trim(this.request.body['script_url']);
-  project.apkUrl = _.trim(this.request.body['app_url']);
-  project.statusUrl = _.trim(this.request.body['job_status_url']);
+        project.resultUrl = _.trim(this.request.body['result_url']);
+        project.scriptUrl = _.trim(this.request.body['script_url']);
+        project.apkUrl = _.trim(this.request.body['app_url']);
+        project.statusUrl = _.trim(this.request.body['job_status_url']);
 
-  project.time = _.moment().format('x');
+        project.time = _.moment().format('x');
 
-  project.environment = 'udid='+project.serialNumber;
+        project.environment = 'udid=' + project.serialNumber;
 
-  const attachment = new Attachment();
+        const attachment = new Attachment();
 
-  if (yield project.add()) {
-    co(function* () {
-      console.log(project.apkUrl);
-      var appResult = yield REQUST.get({ url: project.apkUrl+'.md5sum'});
-      var appNames = appResult.headers['content-disposition'].split('\'')[1];
-      var names = Iconv.decode(appResult.headers['content-disposition'].split('\'')[1], 'GBK').toString();
-      var appName = appNames.substring(0,appNames.lastIndexOf("\."))
-      var requestBody = appResult.body;
+        if (yield project.add()) {
+            co(function*() {
+                /**
+                 * 首先检查是否存在 attachment 目录
+                 */
+                var tempDir = path.join(__dirname, '..', '..', '..', '..', 'attachment');
+                if (!fs.existsSync(tempDir)) {
+                    _.mkdir(tempDir);
+                }
 
-      var tempDir = path.join(__dirname, '..', '..','..','..', 'attachment');
-      if (!fs.existsSync(tempDir)) {
-        _.mkdir(tempDir);
-      }
-      var appDir = path.join(tempDir,"app",requestBody);
+                /**
+                 * 获取脚本文件信息，并且下载脚本文件
+                 */
+                //发送请求到业务系统
+                var scriptResult = yield REQUST.get({ url: project.scriptUrl + '.md5sum' });
+                //获取业务系统返回的 md5 值
+                var scriptRequestBody = scriptResult.body;
+                // var scriptNames = scriptResult.headers['content-disposition'].split('\'')[1];
+                var scriptName = 'script.js';
 
-      /**
-        * 获取脚本文件信息，并且下载脚本文件
-        */
+                console.log(scriptName);
+                //拼接脚本的路径
+                var scriptDir = path.join(tempDir, "script", scriptRequestBody);
+                var thisScript = path.join(scriptDir, scriptName);
 
-      var scriptResult = yield REQUST.get({ url: project.scriptUrl+'.md5sum'});
-      var scriptRequestBody = scriptResult.body;
-      var scriptNames = scriptResult.headers['content-disposition'].split('\'')[1];
-      var scriptName = scriptNames.substring(0,scriptNames.lastIndexOf("\."))
-      console.log(scriptName);
-      var scriptDir = path.join(tempDir,"script",scriptRequestBody);
-      var thisScript =  path.join(scriptDir,scriptName);
-      if (!fs.existsSync(scriptDir)) {
-        _.mkdir(scriptDir);
-        request(project.scriptUrl).pipe(
-              fs.createWriteStream(thisScript)
-            );
+                if (!fs.existsSync(scriptDir)) {
+                    _.mkdir(scriptDir);
+                    request(project.scriptUrl).pipe(
+                        fs.createWriteStream(thisScript)
+                    );
+                    //设置 attachment的属性值
+                    attachment.attachmentScriptPath = scriptDir;
+                    attachment.attachmentScriptName = scriptName;
+                } else {
+                    attachment.attachmentScriptPath = scriptDir;
+                    attachment.attachmentScriptName = scriptName;
+                }
 
-        attachment.attachmentScriptPath=scriptDir;
-        attachment.attachmentScriptName=scriptName;
-      }else{
-        attachment.attachmentScriptPath=scriptDir;
-        attachment.attachmentScriptName=scriptName;
-      }
-      attachment.projectId = project._id;
-      if (!fs.existsSync(appDir)) {
-        _.mkdir(appDir);
-        var thisApp = path.join(appDir,appName);
-        request(project.apkUrl).pipe(
-              fs.createWriteStream(thisApp)
-            );
-        attachment.attachmentAppPath=appDir;
-        attachment.attachmentAppName=appName;
+                /**
+                 * 获取app的md5值，并根据md5值进行判断是否下载app
+                 *
+                 */
+                console.log(project.apkUrl);
+                //向业务系统发送请求，请求获取app的md5值
+                var appResult = yield REQUST.get({ url: project.apkUrl + '.md5sum' });
+                //获取业务系统返回的值
+                var appNames1 = appResult.headers['content-disposition'];
+                console.log(appNames1);
+                var appNames = appResult.headers['content-disposition'].split('\'')[1];
+                console.log(appNames);
+                var appName = appNames.substring(0, appNames.lastIndexOf("\."));
+                console.log(appName);
+                var apk = appName.split('\.');
+                //由于android和IOS的app后缀名不同，所以分开要区分
+                appName = 'app.' + apk[1];
+                console.log(appName);
+                var requestBody = appResult.body;
+                var appDir = path.join(tempDir, "app", requestBody);
+
+                attachment.projectId = project._id;
+                if (!fs.existsSync(appDir)) {
+                    _.mkdir(appDir);
+                    var thisApp = path.join(appDir, appName);
+                    request(project.apkUrl).pipe(
+                        fs.createWriteStream(thisApp)
+                    );
+                    attachment.attachmentAppPath = appDir;
+                    attachment.attachmentAppName = appName;
 
 
-        if(yield attachment.add()){
-          this.body = {
-            success: true,
-            errorMsg: null,
-            data: null
-              };
-          } else {
-            this.body = {
-            success: false,
-            errorMsg: '提交任务失败',
-            data: null
-             };
-          }
-       }else{
-         attachment.attachmentAppPath=appDir;
-         attachment.attachmentAppName=appName;
-         if(yield attachment.add()){
-           this.body = {
-             success: true,
-             errorMsg: null,
-             data: null
-               };
-           } else {
-             this.body = {
-             success: false,
-             errorMsg: '提交任务失败',
-             data: null
-              };
-           }
+                  if(yield attachment.add()){
+                    this.body = {
+                      success: true,
+                      errorMsg: null,
+                      data: null
+                        };
+                    } else {
+                      this.body = {
+                      success: false,
+                      errorMsg: '提交任务失败',
+                      data: null
+                       };
+                    }
+            }else{
+               attachment.attachmentAppPath=appDir;
+               attachment.attachmentAppName=appName;
+             if(yield attachment.add()){
+               this.body = {
+                 success: true,
+                 errorMsg: null,
+                 data: null
+                   };
+               } else {
+                 this.body = {
+                 success: false,
+                 errorMsg: '提交任务失败',
+                 data: null
+                  };
+               }
        }
 
     }).catch(function (err) {
